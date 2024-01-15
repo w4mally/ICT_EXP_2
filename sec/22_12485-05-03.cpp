d87eae6ab36997e757452466e8d814116abf946a
w4mally <kazuma246r@gmail.com>
2024-01-15 17:29:56 +0900

    create03

---
+++ sec/22_12485-05-03.cpp
@@ -0,0 +1,14 @@
+// プログラミング言語:C++
+// コンパイル方法:g++ -std=c++17 22_12485-05-03.cpp
+// 実行方法:コンパイルで生成したa.outというファイルを実行
+// (Ubuntu 20.04.06 LTSで"./a.out"で実行できることを確認した)
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main(void){
+    return 0;
+}
